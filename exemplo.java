4abd179dd8a6e2b119f07bb3e35f20e96e666435
Silas Assis <silas.24y@gmail.com>
2026-02-24 22:06:05 -0300

    Add files via upload

---
+++ exemplo.java
@@ -0,0 +1,6 @@
+public class exemplo{ 
+    public static void main (String [] args) {
+        System.out.println("Minha banda favorita é Charlie Brown Jr");
+        System.out.println("Eu torço para o São Paulo");
+    }
+}
